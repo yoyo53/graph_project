9b740daa3726cd621d983a431ab30b86e1a3d0be
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-03-11 18:05:05 +0100

    commit

--- graph.py
+++ graph.py
@@ -7,25 +7,22 @@ class Graph:
     def __init__(self, filepath):
         self.vertices = []
         self.edges = []
-        self.is_directed = True
 
         with open(filepath) as file:
             lines = file.readlines()
             lines = [line.replace("\n", "").strip(" ") for line in lines]
             lines = list(filter(None, lines))
 
-            source = Vertex("0")
-            self.vertices.append(source)
-            for i in range(1, len(lines) + 1):
-                self.vertices.append(Vertex(str(i)))
-            destination = Vertex(str(len(lines) + 1))
-            self.vertices.append(destination)
+            self.vertices.append(Vertex("0"))
+            for i in range(len(lines)):
+                self.vertices.append(Vertex(str(i + 1)))
+            self.vertices.append(Vertex(str(len(lines) + 1)))
 
             for line in lines:
                 line = line.split(" ")
                 task = next(filter(lambda obj: obj.name == line[0], self.vertices))
                 if len(line) == 2:
-                    self.edges.append(Edge(source, task, 0))
+                    self.edges.append(Edge(self.vertices[0], task, 0))
                 else:
                     for constraint in line[2:]:
                         source = next(filter(lambda obj: obj.name == constraint, self.vertices))
@@ -33,23 +30,22 @@ class Graph:
                         self.edges.append(Edge(source, task, int(duration)))
 
             for vertex in self.vertices:
-                if vertex != destination and next(filter(lambda obj: obj.source == vertex, self.edges), None) is None:
+                if vertex != self.vertices[-1] and next(filter(lambda obj: obj.source == vertex, self.edges), None) is None:
                     duration = next(filter(lambda line: line.split(" ")[0] == vertex.name, lines)).split(" ")[1]
-                    self.edges.append(Edge(vertex, destination, duration))
+                    self.edges.append(Edge(vertex, self.vertices[-1], int(duration)))
 
     def __str__(self):
-        lines = [f'{len(self.vertices)} vertices', f'{len(self.edges)} edges']
+        lines = [f"{len(self.vertices)} vertices", f"{len(self.edges)} edges"]
         for edge in self.edges:
             lines.append(str(edge))
-        return '\n'.join(lines)
+        return "\n".join(lines)
 
     def as_matrix(self):
         matrix = [[""] + [vertex.name for vertex in self.vertices]]
         for source in self.vertices:
             matrix.append([source.name])
-            for i in range(1, len(matrix[0])):
-                destination = next(filter(lambda obj: obj.name == matrix[0][i], self.vertices))
-                edge = next(filter(lambda obj: obj.source == source and obj.target == destination, self.edges), None)
+            for target in self.vertices:
+                edge = next(filter(lambda obj: obj.source == source and obj.target == target, self.edges), None)
                 if edge is None:
                     matrix[-1].append("*")
                 else:
@@ -57,5 +53,5 @@ class Graph:
 
         sizes = [max(map(len, col)) for col in zip(*matrix)]
         fmt = " | ".join(f"{{:{size}}}" for size in sizes)
-        table = [fmt.format(*row) for row in matrix]
-        return "\n".join(table)
+        lines = [fmt.format(*row) for row in matrix]
+        return "\n".join(lines)
