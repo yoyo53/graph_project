ba615f7ad44a83dc83624cab7a0633ceb49cf7fe
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-03-18 12:42:05 +0100

    improved interface

--- interface.py
+++ interface.py
@@ -15,15 +15,14 @@ class Interface(Tk):
         Label(self, fg='black', font=('Arial', 16), justify="center", text="Graph Theory project").pack(pady=10)
         self.content = Frame(self)
         self.content.place(relx=.5, rely=.5, anchor=CENTER)
-        self.table_frame = Frame(self.content)
-        self.table_frame.pack(pady=10)
-        Label(self.table_frame, fg='black', font=('Arial', 16), justify="center", text="No graph selected").pack()
+        self.graph_frame = Frame(self.content)
+        self.graph_frame.grid(row=0, pady=10)
+        Label(self.graph_frame, fg='black', font=('Arial', 16), justify="center", text="No graph selected").pack()
         self.analyze_frame = Frame(self.content)
-        self.analyze_frame.pack(pady=10)
         self.button_frame = Frame(self.content)
-        self.button_frame.pack()
+        self.button_frame.grid(row=2, pady=10)
         Button(self.button_frame, text='Load graph', command=self.load_graph).grid(row=0, column=0, padx=10)
-        Button(self.button_frame, text='Analyze graph', command=self.analyze_graph).grid(row=0, column=1, padx=10)
+        self.analyze_button = Button(self.button_frame, text='Analyze graph', command=self.analyze_graph)
 
     def load_graph(self):
         self.filepath = askopenfilename(filetypes=[("text files", "*.txt")], initialdir="./test files/")
@@ -34,32 +33,67 @@ class Interface(Tk):
             self.add_graph_table()
 
     def add_graph_table(self):
-        for child in self.table_frame.winfo_children():
+        for child in self.graph_frame.winfo_children():
             child.destroy()
+        self.analyze_frame.grid_forget()
+        self.analyze_button.grid(row=0, column=1, padx=10)
+        Label(self.graph_frame, fg='black', font=('Arial', 12), justify="center", text=f"Graph from file {self.filepath.split('/')[-1]}").grid(row=0, column=0, pady=10, columnspan=2)
         matrix = self.graph.as_matrix()
         sizes = [max(map(len, col)) + 3 for col in zip(*matrix)]
+        table_frame = Frame(self.graph_frame)
+        table_frame.grid(row=1, column=0, padx=10)
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] != "*":
                     color = 'blue'
                 else:
                     color = 'black'
-                Label(self.table_frame, width=sizes[j], fg=color, font=('Arial', 12), justify="center", text=matrix[i][j], relief=RAISED).grid(row=i, column=j)
+                Label(table_frame, width=sizes[j], fg=color, font=('Arial', 12), justify="center", text=matrix[i][j], relief=RAISED).grid(row=i, column=j)
+        as_list = str(self.graph).split("\n")
+        max_rows = int(1.4 * len(self.graph.vertices))
+        if len(as_list) > max_rows:
+            for i in range(0, len(as_list), max_rows):
+                Label(self.graph_frame, fg='black', font=('Arial', 12), justify="left", text="\n".join(as_list[i:i+max_rows])).grid(row=1, column=int(1 + i/max_rows), sticky=NW, padx=10)
+        else:
+            Label(self.graph_frame, fg='black', font=('Arial', 12), justify="left", text=self.graph).grid(row=1, column=1, sticky=NW, padx=10)
 
     def analyze_graph(self):
-        if self.graph is None:
-            showinfo("Warning", "No Graph selected! Select a graph to be able to analyze it.")
+        self.analyze_frame.grid(row=1)
+        for child in self.analyze_frame.winfo_children():
+            child.destroy()
+        self.analyze_button.grid_forget()
+        if not self.graph.is_scheduling():
+            lines = [
+                "This is not a scheduling graph. A scheduling graph must satisfies the following conditions:",
+                "    - a single entry point,",
+                "    - a single exit point,",
+                "    - no cycle,",
+                "    - same weights for all outgoing edges of a vertex,",
+                "    - outgoing edges of the entry vertex have zero,",
+                "    - no negative edges."
+            ]
+            Label(self.analyze_frame, fg='black', font=('Arial', 12), justify="left", text="\n".join(lines)).pack()
         else:
-            for child in self.analyze_frame.winfo_children():
-                child.destroy()
+            lines = [
+                "This is a scheduling graph with no cycle and no negative edges."
+            ]
             matrix = [["vertex"] + [vertex.name for vertex in self.graph.vertices]]
-            print(matrix)
             matrix += [["rank"] + [str(rank) for rank in self.graph.get_rank()]]
             matrix += [["earliest date"] + [str(date) for date in self.graph.get_earliest_date()]]
             matrix += [["latest date"] + [str(date) for date in self.graph.get_latest_date()]]
             matrix += [["total float"] + [str(Float) for Float in self.graph.get_total_float()]]
             matrix += [["free float"] + [str(Float) for Float in self.graph.get_free_float()]]
             sizes = [max(map(len, col)) + 2 for col in zip(*matrix)]
+            table_frame = Frame(self.analyze_frame)
+            table_frame.grid(row=0, column=0, padx=10)
             for i in range(len(matrix)):
                 for j in range(len(matrix[0])):
-                    Label(self.analyze_frame, width=sizes[j], fg='blue', font=('Arial', 12), justify="center", text=matrix[i][j], relief=RAISED).grid(row=i, column=j)
+                    Label(table_frame, width=sizes[j], fg='blue', font=('Arial', 12), justify="center", text=matrix[i][j], relief=RAISED).grid(row=i + 1, column=j)
+            critical_paths = self.graph.get_critical_path()
+            if len(critical_paths) > 1:
+                lines += ["The critical paths are:"]
+            else:
+                lines += ["The critical path is:"]
+            for path in self.graph.get_critical_path():
+                lines.append("    - " + " --> ".join(str(vertex) for vertex in path))
+            Label(self.analyze_frame, fg='black', font=('Arial', 12), justify="left", text="\n".join(lines)).grid(row=0, column=1, pady=10, sticky=NW)
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 from graph import Graph
 from interface import Interface
 
-graph = Graph('test files/test2.txt')
+graph = Graph('test files/test3.txt')
 print(graph)
 print(graph.as_formatted_matrix())
-print(graph.get_earliest_date())
-print(graph.get_latest_date())
-print(graph.get_rank())
-print(graph.get_total_float())
-print(graph.get_free_float())
-paths = graph.get_critical_path()
-print("Critical paths:")
-for path in paths:
-    print("- " + " --> ".join(str(cell) for cell in path))
+
+if not graph.is_scheduling():
+    lines = [
+        "This is not a scheduling graph. A scheduling graph must satisfies the following conditions:",
+        "    - a single entry point,",
+        "    - a single exit point,",
+        "    - no cycle,",
+        "    - same weights for all outgoing edges of a vertex,",
+        "    - outgoing edges of the entry vertex have zero,",
+        "    - no negative edges."
+    ]
+    print("\n".join(lines))
+else:
+    print("This is a scheduling graph with no cycle and no negative edges.")
+    matrix = [["vertex"] + [vertex.name for vertex in graph.vertices]]
+    matrix += [["rank"] + [str(rank) for rank in graph.get_rank()]]
+    matrix += [["earliest date"] + [str(date) for date in graph.get_earliest_date()]]
+    matrix += [["latest date"] + [str(date) for date in graph.get_latest_date()]]
+    matrix += [["total float"] + [str(Float) for Float in graph.get_total_float()]]
+    matrix += [["free float"] + [str(Float) for Float in graph.get_free_float()]]
+    sizes = [max(map(len, col)) for col in zip(*matrix)]
+    fmt = " | ".join(f"{{:{size}}}" for size in sizes)
+    lines = [fmt.format(*row) for row in matrix]
+    print("\n".join(lines))
+    critical_paths = graph.get_critical_path()
+    if len(critical_paths) > 1:
+        print("The critical paths are:")
+    else:
+        print("The critical path is:")
+    for path in graph.get_critical_path():
+        print("    - " + " --> ".join(str(vertex) for vertex in path))
 
 if __name__ == '__main__':
     interface = Interface()
